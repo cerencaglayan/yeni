033d4452411e86c88e174e5ab5f7512c3d5c723c
cerencaglayan <116206278+cerencaglayan@users.noreply.github.com>
2025-12-04 13:20:00 +0300

    -

--- Controllers/SampleController.cs
+++ Controllers/SampleController.cs
@@ -14,7 +14,7 @@ namespace yeni.Controllers
                 message = "Hello from SampleController!",
                 timestamp = DateTime.UtcNow
             });
-        }
+        } 
     }
     
     
